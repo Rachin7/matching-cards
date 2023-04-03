dcda63707be7db808cbad7a0db3b2b143307b00c
Rachin7 <djrachin123@gmail.com>
2023-04-03 11:46:01 -0400

    attempt to create card element

--- js/app.js
+++ js/app.js
@@ -5,3 +5,19 @@ const blockNum = cardsPicklist.length
 
 let revealedNum = 0
 let activeBlock = null
+let awaitingMovement = false
+
+function buildBlock(card) {
+  const element = document.createElement('div')
+  element.classList.add('block')
+  element.setAttribute('data-card', card)
+  return element
+}
+
+for (let i = 0; i < blockNum; i++) {
+  let randomIndx = Math.floor(Math.random() * cardsPicklist.length)
+  let card = cardsPicklist[randomIndx]
+
+  cardsPicklist.splice(randomIndx, 1)
+  console.log(card)
+}
